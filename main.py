80aa29ec1b93a096c88623662c608bb94cafcee8
Prerna <prernakumarisharma2003@gmail.com>
2026-04-20 21:40:28 +0530

    urls name changed

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ class URL(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/url_check")
+@app.post("/shorten")
 async def url_shorten(url:URL):
     short_code= secrets.token_urlsafe(5)
     urls[short_code] = url.url
     return {"short_code":short_code, "short_url": "http://localhost:8000/"+short_code}
 
-@app.get("/url_check/{code}")
+@app.get("/shorten/{code}")
 async def get_code( code):
     if code in urls:
         code_url= urls.get(code)
