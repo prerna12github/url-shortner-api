fcc96f4d25ae2751f989b6e28dbc6e9352c0c101
Prerna <prernakumarisharma2003@gmail.com>
2026-05-31 13:35:58 +0530

    fix

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import secrets
+from urllib.parse import urlparse
+
+import starlette.status as status
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
-from fastapi.middleware.cors import CORSMiddleware
+
 from api.database import get_code, get_record, set_record
-import starlette.status as status
-from urllib.parse import urlparse
 
 app = FastAPI()
 
 app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-    
+
+
 @app.get("/{short_code}")
 def redirect_url(short_code: str):
     record = get_record(short_code)
     if record:
-        return RedirectResponse(url=record,status_code=status.HTTP_302_FOUND)
+        return RedirectResponse(url=record, status_code=status.HTTP_302_FOUND)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
@@ -33,12 +38,12 @@ def redirect_url(short_code: str):
 async def shorten_url(url: str):
     result = urlparse(url)
     if not result.scheme or not result.netloc:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid URL")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid URL"
+        )
     code = await get_code(url)
     if code:
-        return f"{code}"         
+        return f"{code}"
     short_code = secrets.token_urlsafe(5)
     set_record(url, short_code)
     return f"{short_code}"
-
-    
